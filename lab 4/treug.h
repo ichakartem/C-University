f7cde0c63428e435c6d0f303c5b29ea815953c68
ichakartem <ichakartem@gmail.com>
2026-03-21 13:35:34 +1000

    Create treug.h

---
+++ lab 4/treug.h
@@ -0,0 +1,10 @@
+#ifndef TREUG_H
+#define TREUG_H
+#include <stdbool.h>
+
+bool is_valid_sides(double a, double b, double c);
+bool is_triangle(double a, double b, double c);
+double triangle_perimeter(double a, double b, double c);
+double triangle_area(double a, double b, double c);
+
+#endif
