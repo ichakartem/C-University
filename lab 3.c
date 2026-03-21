b7bf803d0867a846f5c992fbc73707508093654f
ichakartem <ichakartem@gmail.com>
2026-03-21 10:41:47 +1000

    Update lab 3.c

--- lab 3.c
+++ lab 3.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
-#include <math.h>
 #include <string.h>
 #include <ctype.h>
 
 int main(void) {
     char s[81];
-    scanf("%80s", s);
-    
+    scanf("%80[^\n]", s);
     char s2[35];
     scanf("%34s", s2);
-    
+
     printf("длина строки %zu: \n ", strlen(s));
     for (int i = 0; i < strlen(s2); i++){
         for (int j = 0; j<strlen(s); j++){
             if (s2[i] == s[j]) {
                 s[j] = toupper(s2[i]);
             }
+
+            if (s[j] == '!') {
+                s[j] = '?';
+            }
         }
     }
+
     printf("%s \n", s);
     return 0;
 }
